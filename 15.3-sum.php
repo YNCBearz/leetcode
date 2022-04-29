7e9deaca1fb9ffb2245b0aa8d04f1ea54728b719
YNCBearz <bear19930414@gmail.com>
2022-04-30 00:37:58 +0800

    3sum寫法待研究

--- 15.3-sum.php
+++ 15.3-sum.php
@@ -21,63 +21,17 @@ class Solution
     public function threeSum($nums)
     {
         $answer = [];
-        if (count($nums) < 3) {
+        $length = count($nums);
+
+        if ($length < 3) {
             return $answer;
         }
 
-        $keyMapping = [];
-        foreach ($nums as $key => $value) {
-            if (isset($keyMapping[$value])) {
-                $keyMapping[$value][] = $key;
-                continue;
-            }
-
-            $keyMapping[$value][] = $key;
-        }
-
-        $windowLeft = 0;
-        $windowRight = 1;
-
-        while ($windowLeft < count($nums) - 2) {
-            while ($windowRight < count($nums)) {
-                $total = $nums[$windowLeft] + $nums[$windowRight];
-
-                $goal = 0 - $total;
-
-                if (isset($keyMapping[$goal])) {
-                    foreach ($keyMapping[$goal] as $key) {
-                        if (
-                            $key != $windowLeft &&
-                            $key != $windowRight
-                        ) {
-                            $matchKeys = [$windowLeft, $windowRight, $key];
-                            sort($matchKeys);
-
-                            $answerValue = [
-                                $nums[$matchKeys[0]],
-                                $nums[$matchKeys[1]],
-                                $nums[$matchKeys[2]],
-                            ];
-                            sort($answerValue);
-
-                            $answerKey = implode('-', $answerValue);
-                            $answer[$answerKey] = $answerValue;
-                        }
-                    }
-                }
-
-                $windowRight++;
-            }
-
-            $windowLeft++;
-            $windowRight = $windowLeft + 1;
-        }
-
-        return $answer;
+        sort($nums);
     }
 }
 // @lc code=end
 
-// $solution = new Solution();
-// $answer = $solution->threeSum([-1, 0, 1, 2, -1, -4]);
+// $Solution = new Solution();
+// $answer = $Solution->threeSum([-1, 0, 1, 2, -1, -4]);
 // dump($answer);
