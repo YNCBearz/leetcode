0305c88102626d963e16f4f2b32f8bb920c98659
Bear <bear.hsiung@drsays.org>
2022-05-24 17:03:08 +0800

    待修正

--- 460.lfu-cache.php
+++ 460.lfu-cache.php
@@ -9,11 +9,82 @@ require __DIR__ . '/vendor/autoload.php';
  *
  * [460] LFU Cache
  */
+class Node
+{
+    public ?int $key;
+    public ?int $value;
+    public int $frequence;
+    public ?Node $prev;
+    public ?Node $next;
+
+    public function __construct(
+        ?int $key,
+        ?int $value
+    ) {
+        $this->key = $key;
+        $this->value = $value;
+
+        $this->frequence = 1;
+        $this->prev = null;
+        $this->next = null;
+    }
+}
+
+class DoubleLinkedList
+{
+    protected Node $sentinel;
+    public int $size;
+
+    public function __construct()
+    {
+        $this->sentinel = new Node(null, null);
+        $this->sentinel->next = $this->sentinel;
+        $this->sentinel->prev = $this->sentinel;
+
+        $this->size = 0;
+    }
+
+    public function append(Node $node)
+    {
+        $node->prev = $this->sentinel;
+        $node->next = $this->sentinel->next;
+
+        $this->sentinel->next->prev = $node;
+        $this->sentinel->next = $node;
+
+        $this->size++;
+    }
+
+    public function pop(?Node $node = null): ?Node
+    {
+        if ($this->size == 0) {
+            return null;
+        }
+
+        //nice
+        if (is_null($node)) {
+            $node = $this->sentinel->prev;
+        }
+
+        $node->prev->next = $this->sentinel;
+        $this->sentinel->prev = $node->prev;
+
+        $this->size--;
+
+        return $node;
+    }
+}
 
 // @lc code=start
 class LFUCache
 {
-    protected int $capacity = 0;
+    protected int $capacity;
+    protected int $size;
+    protected array $nodeDictionary;
+    protected array $doubleListCollection;
+    protected int $minFrequence;
+
+    const NODE_NOT_EXISTS = -1;
 
     /**
      * @param int $capacity
@@ -21,6 +92,10 @@ class LFUCache
     public function __construct($capacity)
     {
         $this->capacity = $capacity;
+        $this->size = 0;
+        $this->nodeDictionary = [];
+        $this->doubleListCollection = [];
+        $this->minFrequence = 0;
     }
 
     /**
@@ -29,6 +104,35 @@ class LFUCache
      */
     public function get($key)
     {
+        if (!isset($this->nodeDictionary[$key])) {
+            return self::NODE_NOT_EXISTS;
+        }
+
+        $node = $this->nodeDictionary[$key];
+        $frequence = $node->frequence;
+
+        if (!isset($this->doubleListCollection[$frequence])) {
+            $this->doubleListCollection[$frequence] = new DoubleLinkedList();
+        }
+
+        $doubleLinkedList = $this->doubleListCollection[$frequence];
+        $doubleLinkedList->pop($node);
+
+        if ($doubleLinkedList->size == 0 && $frequence == $this->minFrequence) {
+            $this->minFrequence++;
+        }
+
+        $node->frequence++;
+        $frequence = $node->frequence;
+
+        if (!isset($this->doubleListCollection[$frequence])) {
+            $this->doubleListCollection[$frequence] = new DoubleLinkedList();
+        }
+
+        $doubleLinkedList = $this->doubleListCollection[$frequence];
+        $doubleLinkedList->append($node);
+
+        return $node->value;
     }
 
     /**
@@ -38,6 +142,58 @@ class LFUCache
      */
     public function put($key, $value)
     {
+        if ($this->capacity == 0) {
+            return;
+        }
+
+        if (isset($this->nodeDictionary[$key])) {
+            $node = $this->nodeDictionary[$key];
+            $node->value = $value;
+
+            $node = $this->nodeDictionary[$key];
+            $frequence = $node->frequence;
+
+            $doubleLinkedList = $this->doubleListCollection[$frequence];
+            $doubleLinkedList->pop($node);
+
+            if ($doubleLinkedList->size == 0 && $frequence == $this->minFrequence) {
+                $this->minFrequence++;
+            }
+
+            $node->frequence++;
+            $frequence = $node->frequence;
+            $doubleLinkedList = $this->doubleListCollection[$frequence];
+            $doubleLinkedList->append($node);
+
+            return;
+        }
+
+        if ($this->size == $this->capacity) {
+            $doubleLinkedList = $this->doubleListCollection[$this->minFrequence];
+            $node = $doubleLinkedList->pop();
+
+            $key = $node->key;
+            unset($this->nodeDictionary[$key]);
+
+            $this->size--;
+
+            if ($doubleLinkedList->size == 0 && $this->minFrequence > 0) {
+                $this->minFrequence--;
+            }
+        }
+
+        $node = new Node($key, $value);
+        $this->nodeDictionary[$key] = $node;
+
+        if (!isset($this->doubleListCollection[1])) {
+            $this->doubleListCollection[1] = new DoubleLinkedList();
+        }
+
+        $doubleLinkedList = $this->doubleListCollection[1];
+        $doubleLinkedList->append($node);
+
+        $this->minFrequence = 1;
+        $this->size++;
     }
 }
 
