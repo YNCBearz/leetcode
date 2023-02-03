4e055369fc73e01d681fbbb9fcac3a0199608596
Bear Hsiung <bear.hsiung@titansoft.com.sg>
2023-02-03 10:40:36 +0800

    feat: 9.palindrome-number (use algorithm)

--- 9.palindrome-number.cs
+++ 9.palindrome-number.cs
@@ -6,19 +6,32 @@
 
 // @lc code=start
 using System;
+using System.Runtime.Intrinsics.X86;
 
 public class Solution {
     public bool IsPalindrome(int i) {
-        var x = i.ToString();
-        var y = StringReverse(x);
+        if (i == 0)
+        {
+            return true;
+        }
 
-        return x == y;
-    }
+        if (i < 0 || i % 10 == 0)
+        {
+            return false;
+        }
 
-    private string StringReverse(string s) {
-        char[] ch = s.ToCharArray();
-        Array.Reverse(ch);
-        return new String(ch);
+        var revertedNumber = 0;
+        while (i > revertedNumber)
+        {
+            revertedNumber = revertedNumber * 10 + i % 10;
+            i = i/10;
+        }
+
+		// When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+		// For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
+		// since the middle digit doesn't matter in palidrome(it will always equal to itself), we can simply get rid of it.
+		return i == revertedNumber || i == revertedNumber / 10;
+
     }
 }
 // @lc code=end
